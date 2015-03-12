46f5a80a8a9b2a6ac7ba9be692d7a4fbdd031724
Eivind Kristoffersen <eivindskn@hotmail.com>
2015-03-12 16:35:43 -0700

    added hole hit detection

---
+++ MobileImaging/Assets/Scripts/ball.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+}
+
+function OnCollisionEnter(collision : Collision) {
+    // can put sound effect, particle effect
+    Debug.Log("collision!");
+}
+
+function OnTriggerEnter (other : Collider) {
+    Debug.Log("trigger enter");
+    if (other.gameObject.CompareTag("hole")) {
+        Destroy(gameObject);
+    }
+}
